09d7d02e14d8457c94736f7691e85b4e40d59d11
ADOFO-09 <emmanueladofo011@gmail.com>
2024-03-04 12:36:23 +0000

    Linked list

--- collections/Collection_interface.java
+++ collections/Collection_interface.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.TreeSet;
 
 
 public class Collection_interface {
     public static void main(String[] args){
     Collection<String>coll = new ArrayList<String>();
-        Collection<String>coll2 = new TreeSet<String>();
+    Collection<String>coll2 = new TreeSet<String>();
+    LinkedList<String>mylist = new LinkedList<String>();
         coll2.add("Harry");
         coll2.add("Sally");
 
@@ -20,13 +23,48 @@ public class Collection_interface {
         b = coll.contains("Nixon"); // Checks whether this collection contains a given element
 
     
-        String s = coll.toString(); // s is now "[Adofo,Nixon]"
-        System.out.println(coll + " " + n + " " + b);  
+        // String s = coll.toString(); // s is now "[Adofo,Nixon]"
+        // System.out.println(coll + " " + n + " " + b);  
         
         //This loop prints the elements on separate lines.
-        for (String a: coll){ 
-            System.out.println(a);
+        // for (String a: coll){ 
+        //     // System.out.println(a);
+        // }
+
+        // Linked list Methods
+        mylist.addLast("Micheal");
+        mylist.add("Yussif");
+        mylist.addFirst("Emmanuel");
+        System.out.println( mylist.getFirst());
+        System.out.println(mylist.getLast());
+        System.out.println(mylist);
+
+        String removed = mylist.removeFirst();
+        System.out.println(removed);
+        
+        //List Iterator
+        ListIterator<String> iter = mylist.listIterator();
+        iter.next(); // returns a reference to the value of the next element
+        iter.add("Prince");
+        System.out.println(mylist);
+        System.out.println(iter.previous());
+        iter.set("Frank"); // Updates the last element returned by next or previous
+        System.out.println(mylist);
+
+        b = iter.hasNext(); // returns true if there is a next element
+        System.out.println(b);
+        
+        iter.add("Diana"); // Adds an element before the iterator position(ListIterator only)
+
+        if(iter.hasPrevious()){
+           String s = iter.previous();
+           System.out.println(s);
         }
+        System.out.println(mylist);
+        iter.remove(); // Removes the last element returned by next or previous.
+        // You cannot call it immediately after a call to add().It can be called only once after a next or previous.
+        System.out.println(mylist);
+       
         
     }
 }
